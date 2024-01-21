6666b5c27a4ec371c206c5698401ed02c578bae5
AmritoRock <amritokarmokar5i@gmail.com>
2024-01-21 16:09:06 +0600

    r5x: Init Afterlife

--- afterlife_r5x.mk
+++ afterlife_r5x.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Project-Elixir stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common AfterlifeOS stuff
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 
 # Inherit from r5x device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -21,23 +21,7 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := false
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := r5x
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := aosp_r5x
+PRODUCT_NAME := afterlife_r5x
 PRODUCT_MODEL := Realme 5 Series
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
-
-TARGET_VENDOR_PRODUCT_NAME := r5x
-TARGET_VENDOR_DEVICE_NAME := r5x
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="r5x" \
-    PRIVATE_BUILD_DESC="trinket-user 10 QKQ1.200209.002 release-keys"
-
-# Project-Elixir Stuff
-IS_PHONE := true
-ELIXIR_BUILD_TYPE := OFFICIAL
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_STOCK_ACORE := false
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_CALL_RECORDING := true
-TARGET_BUILD_APERTURE_CAMERA := true
